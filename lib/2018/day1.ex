41ce9909b180811d1dc8c4adcb1a7558911fda66
LOU Xun <aquarhead@ela.build>
2018-12-01 19:38:51 +0000

    Add 2018 day 1 part 2

--- lib/2018/day1.ex
+++ lib/2018/day1.ex
@@ -6,4 +6,19 @@ defmodule AoC.Year2018.Day1 do
     |> Enum.map(&String.to_integer/1)
     |> Enum.sum()
   end
+
+  def freq2(input, splitter \\ ",") do
+    input
+    |> String.split(splitter, trim: true)
+    |> Enum.map(&String.trim/1)
+    |> Enum.map(&String.to_integer/1)
+    |> Stream.cycle()
+    |> Enum.reduce_while([0], fn change, [current_freq | _] = acc ->
+      new_freq = current_freq + change
+      case Enum.member?(acc, new_freq) do
+        false -> {:cont, [new_freq | acc]}
+        true -> {:halt, new_freq}
+      end
+    end)
+  end
 end
--- test/2018/day1_test.exs
+++ test/2018/day1_test.exs
@@ -9,4 +9,12 @@ defmodule AoC.Year2018.Day1Test do
     assert 0 == freq("+1, +1, -2")
     assert -6 == freq("-1, -2, -3")
   end
+
+  test "freq2" do
+    assert 2 == freq2("+1, -2, +3, +1")
+    assert 0 == freq2("+1, -1")
+    assert 10 == freq2("+3, +3, +4, -2, -4")
+    assert 5 == freq2("-6, +3, +8, +5, -6")
+    assert 14 == freq2("+7, +7, -2, -7, -4")
+  end
 end
